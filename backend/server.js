004dbcb0bcd885f1f7f7d865282d2039d5b25c06
vimal004 <2004.vimal@gmail.com>
2024-09-04 13:58:01 +0530

    chore: Remove unnecessary code for serving static files

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,5 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-
-// Serve the static files from the Vite build (frontend)
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
