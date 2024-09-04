c658c46c261088c1523f3e484218009a595b8ac0
vimal004 <2004.vimal@gmail.com>
2024-09-04 18:16:52 +0530

    chore: Update server to serve frontend files with Express static middleware

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,6 @@ app.get("/", (req, res) => {
   res.json("Server Running!");
 });
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
