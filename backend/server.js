c0941481e9cdb9a500469b900f991dee857acca7
vimal004 <2004.vimal@gmail.com>
2024-09-04 14:06:03 +0530

    chore: Update server route to return a simple message

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json("Server Running!");
 });
 
 const PORT = process.env.PORT || 3000;
